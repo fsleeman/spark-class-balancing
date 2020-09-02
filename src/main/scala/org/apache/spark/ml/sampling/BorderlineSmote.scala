ae3eb1284d08676f3cd328f240506da3f56863b8
Ford Sleeman <wcsleeman@mymail.vcu.edu>
2020-09-01 23:34:02 -0400

    Added multiclass support for BorderlineSmote

--- src/main/scala/org/apache/spark/ml/sampling/BorderlineSmote.scala
+++ src/main/scala/org/apache/spark/ml/sampling/BorderlineSmote.scala
@@ -59,7 +59,7 @@ class BorderlineSMOTEModel private[ml](override val uid: String) extends Model[B
 
   def getNewSample(current: DenseVector, i: DenseVector, range: Double) : DenseVector = {
     val xx: Array[Array[Double]] = Array(current.values, i.values)
-    val sample: Array[Double] = xx.transpose.map(x=>x(0) + Random.nextDouble * range * (x(1)-x(0)))
+    val sample: Array[Double] = xx.transpose.map(x=>x(0) + Random.nextDouble * range * (x(1)+x(0)))
     Vectors.dense(sample).toDense
   }
 
@@ -70,12 +70,11 @@ class BorderlineSMOTEModel private[ml](override val uid: String) extends Model[B
     val rows = selected.map(x=>getNewSample(current, x, range)).map(x=>Row(0, label, x))
     rows.toArray
   }
-  
 
-  override def transform(dataset: Dataset[_]): DataFrame = {
+   def transform2(dataset: Dataset[_]): DataFrame = {
     val mode = 1 // FIXME - add parameter
-    
-    val df = dataset.filter((dataset("label") === 1) || (dataset("label") === 5)).toDF // FIXME
+
+    val df = dataset.toDF // .filter((dataset("label") === 1) || (dataset("label") === 5)).toDF // FIXME
     import df.sparkSession.implicits._
 
     val m = 5   // k-value
@@ -87,12 +86,36 @@ class BorderlineSMOTEModel private[ml](override val uid: String) extends Model[B
     val maxClassLabel = counts.orderBy(desc("_2")).take(1)(0)(0).toString
     val maxClassCount = counts.orderBy(desc("_2")).take(1)(0)(1).toString.toInt
 
+
+    val clsList: Array[Int] = counts.select("_1").filter(counts("_1") =!= maxClassLabel).collect().map(x=>x(0).toString.toInt)
+    // val clsDFs = clsList.indices.map(x=>oversample(df.filter(df("label")===clsList(x)), maxClassCount))
+
+    println("Majority class: " + maxClassLabel)
+    for(x<-clsList) {
+      println("cls: " + x)
+    }
+
     val samplesToAdd = maxClassCount - minClassCount
+
+    dataset.toDF
+
+  }
+
+  def oversampleClass(dataset: Dataset[_], minorityClassLabel: String, samplesToAdd: Int): DataFrame ={
+    val df = dataset.toDF //filter((dataset("label") === minorityClassLabel) || (dataset("label") === 4) || (dataset("label") === 2) || (dataset("label") === 3) || (dataset("label") === 5) || (dataset("label") === 6) || (dataset("label") === 7)).toDF // FIXME
+    import df.sparkSession.implicits._
+    val m = 5   // k-value
+    val leafSize = 1000
+
+    //val samplesToAdd = maxClassCount - minClassCount
     println("Samples to add: " + samplesToAdd)
+    // println("minority count: " + minClassCount)
+
 
     // step 1
-    /*** For each minority example, calculate the m nn's in training set***/
-    val minorityDF = df.filter(df("label")===minClassLabel)
+    //  For each minority example, calculate the m nn's in training set
+    val minorityDF = df.filter(df("label")===minorityClassLabel)
+
     val model = new KNN().setFeaturesCol("features")
       .setTopTreeSize(df.count().toInt / 8)   /// FIXME - check?
       .setTopTreeLeafSize(leafSize)
@@ -101,11 +124,11 @@ class BorderlineSMOTEModel private[ml](override val uid: String) extends Model[B
       .setAuxCols(Array("label", "features"))
 
     val f = model.fit(df)
-    val t = f.transform(minorityDF) // FIXME - check if the nearest neighbor should be ignored or not
+    val t = f.transform(minorityDF)
 
 
     // step 2
-    /*** Find DANGER examples: if m'=m (noise), m/2 < m' < m (DANGER), 0 <= m' <= m/2 (safe)***/
+    // Find DANGER examples: if m'=m (noise), m/2 < m' < m (DANGER), 0 <= m' <= m/2 (safe)
     t.show()
     t.printSchema()
 
@@ -113,27 +136,27 @@ class BorderlineSMOTEModel private[ml](override val uid: String) extends Model[B
     println("Is danger")
     val dfDanger = t.filter(isDanger(t("neighbors").getItem("label")))
     dfDanger.show()
-    println(minorityDF.count())
-    println(dfDanger.count)
+    println("minorityDF:" + minorityDF.count())
+    println("danger count:" + dfDanger.count)
 
-    val s = 3
+    println("s value: " + samplesToAdd / dfDanger.count.toDouble)
+    val s = (samplesToAdd / dfDanger.count.toDouble).ceil.toInt
+    println(s)
+
 
     // step 3
-    /*** For all DANGER examples, find k nearest examples from minority class***/
+    // For all DANGER examples, find k nearest examples from minority class
     val kValue = 5 /// FIXME - check the k/m values
     val model2 = new KNN().setFeaturesCol("features")
-      .setTopTreeSize(df.count().toInt / 8)   /// FIXME - check?
+      // .setTopTreeSize(df.count().toInt / 8)   /// FIXME - check?
+      .setTopTreeSize(10)   /// FIXME - check this size for small datasets
       .setTopTreeLeafSize(leafSize)
       .setSubTreeLeafSize(leafSize)
       .setK(kValue + 1) // include self example
       .setAuxCols(Array("label", "features"))
-
-    val f2 = model2.fit(df.filter(df("label")===minClassLabel))
+    // val f2 = model2.fit(df.filter(df("label")===minorityClassLabel))
+   val f2 = model2.fit(minorityDF)
     val t2 = f2.transform(dfDanger.drop("neighbors"))
-
-
-
-
 
     // step 4
 
@@ -144,45 +167,33 @@ class BorderlineSMOTEModel private[ml](override val uid: String) extends Model[B
       *  Get rand (0,1) - r for each so s synthetic examples are created using p'i * r(j) * difference(j)
       *
       ***/
-
-    //val temp: Row = t2.take(1)(0)
-    // println(temp)
-
-    //import org.apache.spark.sql._
-
-    //val zz: Int = spark.sparkContext.parallelize(Array(1,2,3)).map(Row(_)).collect()(0).getInt(0)
-    //println("zz " + zz)
-
     val xxxx: DataFrame = t2.select($"neighbors.features")
-    //xxxx.show()
-
-    //xxxx.printSchema()
-
-    val result: Array[Array[Row]] = xxxx.collect.map(row=>generateSamples(row.getValuesMap[Any](row.schema.fieldNames)("features").asInstanceOf[mutable.WrappedArray[DenseVector]], s, 1.0, minClassLabel.toInt))
+    val result: Array[Array[Row]] = xxxx.collect.map(row=>generateSamples(row.getValuesMap[Any](row.schema.fieldNames)("features").asInstanceOf[mutable.WrappedArray[DenseVector]], s, 1.0, minorityClassLabel.toInt))
 
     // mode = 1: borderlineSMOTE1: use minority NNs
     // mode = 2: borderlineSMOTE2: use minority NNs AND majority NNs
-    val fooX: Array[Row] = if(mode == 1) {
-      result.flatMap(x => x.toSeq)
+    val mode = 1 // FIXME - add parameter
+    val fooX: Array[Row] = if(mode == 1) { // FIXME - dont use mode 2, only works directly with binary case
+      val r = scala.util.Random
+      r.shuffle(result.flatMap(x => x.toSeq).toList).take(samplesToAdd).toArray
     }
     else {
-      val modelNegative = new KNN().setFeaturesCol("features")
-        .setTopTreeSize(df.count().toInt / 8)   /// FIXME - check?
+      val modelNegative = new KNN().setFeaturesCol("features") // FIXME - check if the right DFs are used in NNs
+        .setTopTreeSize(df.count().toInt / 8)   /// FIXME - parameter
         .setTopTreeLeafSize(leafSize)
         .setSubTreeLeafSize(leafSize)
         .setK(kValue + 1) // include self example
         .setAuxCols(Array("label", "features"))
 
-      val fNegative = modelNegative.fit(df.filter(df("label")=!=minClassLabel))
+      val fNegative = modelNegative.fit(df.filter(df("label")=!=minorityClassLabel))
       val tNegative = fNegative.transform(dfDanger.drop("neighbors"))
 
       val nearestNegativeExamples: DataFrame = tNegative.select($"neighbors.features")
-      val nearestNegativeSamples: Array[Array[Row]] = nearestNegativeExamples.collect.map(row=>generateSamples(row.getValuesMap[Any](row.schema.fieldNames)("features").asInstanceOf[mutable.WrappedArray[DenseVector]], s, 0.5, minClassLabel.toInt))
+      val nearestNegativeSamples: Array[Array[Row]] = nearestNegativeExamples.collect.map(row=>generateSamples(row.getValuesMap[Any](row.schema.fieldNames)("features").asInstanceOf[mutable.WrappedArray[DenseVector]], s, 0.5, minorityClassLabel.toInt))
       val nearestNegativeRows: Array[Row] = nearestNegativeSamples.flatMap(x=>x.toSeq)
 
       result.flatMap(x => x.toSeq) ++ nearestNegativeRows
     }
-
 
     println("len: " + xxxx.count())
     println("foo: " + fooX.length)
@@ -193,19 +204,47 @@ class BorderlineSMOTEModel private[ml](override val uid: String) extends Model[B
       .withColumnRenamed("_2", "label")
       .withColumnRenamed("_3", "features")
 
-    //bar2.show
-    //bar2.printSchema()
-    //println(bar2.count)
+    //val all = df.union(bar2)
+    //all.show()
+    //println("all: " + all.count())
 
-    val all = df.union(bar2)
-    //println("***********")
-    all.show()
-    println("all: " + all.count())
-    ////val ooo: DataFrame = getCountsByClass(spark, "label", all)
-    //ooo.show
-    //println("***********")
+    //all
+    bar2
+  }
 
-    all
+  override def transform(dataset: Dataset[_]): DataFrame = {
+
+
+
+    val counts = getCountsByClass(dataset.sparkSession, "label", dataset.toDF).sort("_2")
+    val minClassLabel = counts.take(1)(0)(0).toString
+    val minClassCount = counts.take(1)(0)(1).toString.toInt
+    val majorityClassLabel = counts.orderBy(desc("_2")).take(1)(0)(0).toString
+    val majorityClassCount = counts.orderBy(desc("_2")).take(1)(0)(1).toString.toInt
+
+    val minorityClasses = counts.collect.map(x=>(x(0).toString, x(1).toString.toInt)).filter(x=>x._1!=majorityClassLabel)
+    for(x<-minorityClasses) {
+      println("minority label: " + x)
+    }
+
+    val results: DataFrame = minorityClasses.map(x=>oversampleClass(dataset, x._1, majorityClassCount - x._2)).reduce(_ union _).union(dataset.toDF())
+
+//    val results: DataFrame = oversampleClass(dataset, 1.toString, majorityClassCount)
+
+    // val samplesToAdd = maxClassCount - minClassCount
+    // println("samples to add: " + samplesToAdd)
+    // val results: DataFrame = oversampleClass(dataset, 1.toString, samplesToAdd)
+
+    println("dataset: " + dataset.count)
+    println("added: " + results.count)
+    // println("result: " + result.count)
+
+    // val clsList: Array[Int] = counts.select("_1").filter(counts("_1") =!= maxClassLabel).collect().map(x=>x(0).toString.toInt)
+    // val clsDFs = clsList.indices.map(x=>oversample(df.filter(df("label")===clsList(x)), maxClassCount))
+    
+    //
+    results
+
   }
 
   override def transformSchema(schema: StructType): StructType = {
