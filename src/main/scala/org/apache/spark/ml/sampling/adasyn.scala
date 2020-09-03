3cbdbb90b85a9e642aa82d605ae0e072ef70e4aa
Ford Sleeman <wcsleeman@mymail.vcu.edu>
2020-09-02 22:19:12 -0400

    Added multiclass support for ADASYN

--- src/main/scala/org/apache/spark/ml/sampling/adasyn.scala
+++ src/main/scala/org/apache/spark/ml/sampling/adasyn.scala
@@ -54,98 +54,113 @@ class ADASYNModel private[ml](override val uid: String) extends Model[ADASYNMode
   }
 
 
+  def oversampleClass(dataset: Dataset[_], minorityClassLabel: String, samplesToAdd: Int): DataFrame = {
+    val df = dataset.toDF
+    import df.sparkSession.implicits._
+
+    val minorityDF = df.filter(df("label") === minorityClassLabel)
+    val majorityDF = df.filter(df("label") =!= minorityClassLabel)
+    val minorityClassCount = minorityDF.count
+    val majorityClassCount = majorityDF.count
+
+    val threshold = 1.0
+    val beta = 1.0 // final balance level, might need to adjust from the original paper
+
+    val imbalanceRatio = minorityClassLabel.toDouble / majorityClassCount.toDouble
+
+    //val G = (majorityClassCount - minorityClassCount) * beta
+    val G = samplesToAdd
+
+    val leafSize = 100
+    val kValue = 5
+    val model = new KNN().setFeaturesCol("features")
+      .setTopTreeSize(df.count().toInt / 8) /// FIXME - check?
+      .setTopTreeLeafSize(leafSize)
+      .setSubTreeLeafSize(leafSize)
+      .setK(kValue + 1) // include self example
+      .setAuxCols(Array("label", "features"))
+
+    val f: KNNModel = model.fit(df).setDistanceCol("distances")
+    val t = f.transform(minorityDF)
+    t.show
+
+    val getMajorityNeighborRatio = udf((array: mutable.WrappedArray[Int]) => {
+      def isMajorityNeighbor(x1: Int, x2: Int): Int = {
+        if (x1 == x2) {
+          0
+        } else {
+          1
+        }
+      }
+      array.tail.map(x => isMajorityNeighbor(array.head, x)).sum / kValue.toDouble
+    })
+
+
+    val collected = t.select($"neighbors.label")
+    collected.show
+    collected.printSchema()
+
+    val dfNeighborRatio = t.withColumn("neighborClassRatio", getMajorityNeighborRatio($"neighbors.label")).drop("distances") //.drop("neighbors")
+    dfNeighborRatio.show
+
+    val neighborRatioSum = dfNeighborRatio.agg(sum("neighborClassRatio")).first.get(0).toString.toDouble
+    println(neighborRatioSum)
+
+    val getSampleCount = udf((density: Double) => {
+      Math.round(density / neighborRatioSum * G.toDouble)
+    })
+
+    val adjustedRatios = dfNeighborRatio.withColumn("samplesToAdd", getSampleCount($"neighborClassRatio")).withColumn("labels", $"neighbors.label").withColumn("neighborFeatures", $"neighbors.features")
+    adjustedRatios.show
+    adjustedRatios.printSchema()
+
+    val samplesToAddSum = adjustedRatios.agg(sum("samplesToAdd")).first.get(0).toString.toDouble
+    println("majority count: " + majorityClassCount)
+    println("minority count: " + minorityClassCount)
+    println("samples to add: " + samplesToAddSum) // FIXME - double check the size, wont be exactly the right number because of rounding
+
+    adjustedRatios.withColumn("labels", $"neighbors.label").withColumn("neighborFeatures", $"neighbors.features").show
+
+    val syntheticExamples: Array[Array[Array[Double]]] = adjustedRatios.collect.map(x => generateExamples(x))
+
+    println(syntheticExamples.length)
+    val totalExamples = syntheticExamples.flatMap(x => x.toSeq).map(x => (0, minorityClassLabel.toInt, Vectors.dense(x).toDense))
+
+
+    println("total: " + totalExamples.length)
+
+    val bar = df.sparkSession.createDataFrame(df.sparkSession.sparkContext.parallelize(totalExamples))
+    val bar2 = bar.withColumnRenamed("_1", "index")
+      .withColumnRenamed("_2", "label")
+      .withColumnRenamed("_3", "features")
+
+    // df.union(bar2)
+    bar2
+
+  }
+
   override def transform(dataset: Dataset[_]): DataFrame = {
     val df = dataset.toDF()
     import df.sparkSession.implicits._
 
     val counts = getCountsByClass(df.sparkSession, "label", df).sort("_2")
-    val minClassLabel = counts.take(1)(0)(0).toString
-    val minClassCount = counts.take(1)(0)(1).toString.toInt
-    val maxClassLabel = counts.orderBy(desc("_2")).take(1)(0)(0).toString
-    val maxClassCount = counts.orderBy(desc("_2")).take(1)(0)(1).toString.toInt
-
-    println(minClassLabel, minClassCount)
-    println(maxClassLabel, maxClassCount)
-
-    val minorityDF = df.filter(df("label") === minClassLabel)
-    val majorityDF = df.filter(df("label") =!= minClassLabel)
-
-    val threshold = 1.0
-    val beta = 1.0 // final balance level, might need to adjust from the original paper
-
-    val imbalanceRatio = minClassCount.toDouble / maxClassCount.toDouble
-
-    if (imbalanceRatio < threshold) {
-      val G = (maxClassCount - minClassCount) * beta
-
-      val leafSize = 100
-      val kValue = 5
-      val model = new KNN().setFeaturesCol("features")
-        .setTopTreeSize(df.count().toInt / 8) /// FIXME - check?
-        .setTopTreeLeafSize(leafSize)
-        .setSubTreeLeafSize(leafSize)
-        .setK(kValue + 1) // include self example
-        .setAuxCols(Array("label", "features"))
-
-      val f: KNNModel = model.fit(df)
-      val t = f.transform(minorityDF)
-      t.show
-
-      val getMajorityNeighborRatio = udf((array: mutable.WrappedArray[Int]) => {
-        def isMajorityNeighbor(x1: Int, x2: Int): Int = {
-          if (x1 == x2) {
-            0
-          } else {
-            1
-          }
-        }
-
-        array.tail.map(x => isMajorityNeighbor(array.head, x)).sum / kValue.toDouble
-      })
+    // val minClassLabel = counts.take(1)(0)(0).toString
+    // val minClassCount = counts.take(1)(0)(1).toString.toInt
+    val majorityClassLabel = counts.orderBy(desc("_2")).take(1)(0)(0).toString
+    val majorityClassCount = counts.orderBy(desc("_2")).take(1)(0)(1).toString.toInt
 
 
-      val collected = t.select($"neighbors.label")
-      collected.show
-      collected.printSchema()
-      val dfNeighborRatio = t.withColumn("neighborClassRatio", getMajorityNeighborRatio($"neighbors.label")) //.drop("neighbors")
-      dfNeighborRatio.show
+    val minorityClasses = counts.collect.map(x=>(x(0).toString, x(1).toString.toInt)).filter(x=>x._1!=majorityClassLabel)
+    val results: DataFrame = minorityClasses.map(x=>oversampleClass(dataset, x._1, majorityClassCount - x._2)).reduce(_ union _).union(dataset.toDF())
 
-      val neighborCountSum = dfNeighborRatio.agg(sum("neighborClassRatio")).first.get(0).toString.toDouble
-      println(neighborCountSum)
+    println("dataset: " + dataset.count)
+    println("added: " + results.count)
 
-      val getSampleCount = udf((density: Double) => {
-        Math.round(density / neighborCountSum * G.toDouble)
-      })
+    results
 
-      val adjustedRatios = dfNeighborRatio.withColumn("densityDistribution", getSampleCount($"neighborClassRatio")).withColumn("labels", $"neighbors.label").withColumn("neighborFeatures", $"neighbors.features")
-      adjustedRatios.show
-      adjustedRatios.printSchema()
+    // println(minClassLabel, minClassCount)
+    // println(maxClassLabel, maxClassCount)
 
-      val samplesToAddSum = adjustedRatios.agg(sum("densityDistribution")).first.get(0).toString.toDouble
-      println("majority count: " + maxClassCount)
-      println("minority count: " + minClassCount)
-      println("samples to add: " + samplesToAddSum) // FIXME - double check the size, wont be exactly the right number because of rounding
-
-      adjustedRatios.withColumn("labels", $"neighbors.label").withColumn("neighborFeatures", $"neighbors.features").show
-
-      val syntheticExamples: Array[Array[Array[Double]]] = adjustedRatios.collect.map(x => generateExamples(x))
-
-      println(syntheticExamples.length)
-      val totalExamples = syntheticExamples.flatMap(x => x.toSeq).map(x => (0, minClassLabel.toInt, Vectors.dense(x).toDense))
-
-
-      println("total: " + totalExamples.length)
-
-      val bar = df.sparkSession.createDataFrame(df.sparkSession.sparkContext.parallelize(totalExamples))
-      val bar2 = bar.withColumnRenamed("_1", "index")
-        .withColumnRenamed("_2", "label")
-        .withColumnRenamed("_3", "features")
-
-      df.union(bar2)
-
-    } else {
-      df
-    }
   }
 
 
