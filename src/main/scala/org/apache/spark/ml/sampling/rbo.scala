0fe7193780467989811a5bc569b655ae30c83be8
Ford Sleeman <wcsleeman@mymail.vcu.edu>
2020-10-27 22:25:06 -0400

    Added some extra parallelism and parameters

--- src/main/scala/org/apache/spark/ml/sampling/rbo.scala
+++ src/main/scala/org/apache/spark/ml/sampling/rbo.scala
@@ -4,19 +4,57 @@ import org.apache.spark.ml.feature.StringIndexer
 import org.apache.spark.ml.{Estimator, Model}
 import org.apache.spark.ml.linalg.{DenseVector, Vectors}
 import org.apache.spark.ml.param.shared.{HasFeaturesCol, HasSeed}
-import org.apache.spark.ml.param.{ParamMap, Params}
+import org.apache.spark.ml.param.{Param, ParamMap, Params}
 import org.apache.spark.ml.sampling.utilities.{ClassBalancingRatios, HasLabelCol, getSamplesToAdd}
 import org.apache.spark.ml.sampling.utils.getCountsByClass
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.functions.{desc, udf}
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.{DataFrame, Dataset}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+
 import scala.util.Random
 
 
 /** Transformer Parameters*/
 private[ml] trait RBOModelParams extends Params with HasFeaturesCol with HasLabelCol with ClassBalancingRatios {
 
+  /**
+    * Param for gamma
+    * @group param
+    */
+  final val gamma: Param[Double] = new Param[Double](this, "gamma", "gamma value")
+
+  /** @group getParam */
+  final def setGamma(value: Double): this.type = set(gamma, value)
+
+  /**
+    * Param for iterations
+    * @group param
+    */
+  final val iterations: Param[Int] = new Param[Int](this, "iterations", "iterations per synthetic example")
+
+  /** @group getParam */
+  final def setIterations(value: Int): this.type = set(iterations, value)
+
+  /**
+    * Param for stepSize
+    * @group param
+    */
+  final val stepSize: Param[Double] = new Param[Double](this, "stepSize", "stepSize")
+
+  /** @group getParam */
+  final def setStepSize(value: Double): this.type = set(stepSize, value)
+
+  /**
+    * Param for stoppingProbability
+    * @group param
+    */
+  final val stoppingProbability: Param[Double] = new Param[Double](this, "stoppingProbability", "probability of stopping early")
+
+  /** @group getParam */
+  final def setStoppingProbability(value: Double): this.type = set(stoppingProbability, value)
+
+  setDefault(gamma -> 1.0, iterations -> 1, stepSize -> 0.01, stoppingProbability -> 1.0)
 }
 
 /** Transformer */
@@ -29,11 +67,11 @@ class RBOModel private[ml](override val uid: String) extends Model[RBOModel] wit
     difference.sum
   }
 
-  def calculatePhi(x: Array[Double], K: Array[Array[Double]], k: Array[Array[Double]], gamma: Double): Double = {
-    val majorityValue = K.map(Ki=>Math.exp(-Math.pow(pointDifference(Ki, x)/gamma, 2))) /// FIXME - Super slow
-    val minorityValue = k.map(ki=>Math.exp(-Math.pow(pointDifference(ki, x)/gamma, 2))) /// FIXME - Super slow
+  def calculatePhi(spark: SparkSession, x: Array[Double], K: Array[Array[Double]], k: Array[Array[Double]], gamma: Double): Double = {
+    val majorityValue = spark.sparkContext.parallelize(K).map(Ki=>Math.exp(-Math.pow(pointDifference(Ki, x)/gamma, 2))).collect().sum /// FIXME - Super slow
+    val minorityValue = spark.sparkContext.parallelize(k).map(ki=>Math.exp(-Math.pow(pointDifference(ki, x)/gamma, 2))).collect().sum /// FIXME - Super slow
 
-    majorityValue.sum - minorityValue.sum
+    majorityValue - minorityValue
   }
 
   def getRandomStopNumber(numIterations: Int, p: Double) : Int = {
@@ -49,19 +87,20 @@ class RBOModel private[ml](override val uid: String) extends Model[RBOModel] wit
     }
   }
 
-  def createExample(majorityExamples: Array[Array[Double]], minorityExamples: Array[Array[Double]], gamma: Double,
-                    stepSize: Double, numInterations: Int, p: Double): DenseVector = {
+  def createExample(spark: SparkSession, majorityExamples: Array[Array[Double]],
+                    minorityExamples: Array[Array[Double]], gamma: Double, stepSize: Double, numInterations: Int,
+                    p: Double): DenseVector ={
     val featureLength = minorityExamples(0).length
     var point = minorityExamples(Random.nextInt(minorityExamples.length))
 
-    for(x<-0 until getRandomStopNumber(numInterations, p)) {
-      println(x)
+    val pointPhi = calculatePhi(spark, point, majorityExamples, minorityExamples, gamma)
+    for(_ <- 0 until getRandomStopNumber(numInterations, p)) {
       val directions = Set(-1, 1)
       val d: Array[Double] = (0 until featureLength).map(_=>directions.toVector(Random.nextInt(directions.size)).toDouble).toArray
       val v: Array[Double] = (0 until featureLength).map(_=>Random.nextDouble()).toArray
 
       val translated: Array[Double] = Array(point, v, d).transpose.map(x=>x(0) + x(1) * x(2) * stepSize)
-      if(calculatePhi(translated, majorityExamples, minorityExamples, gamma) < calculatePhi(point, majorityExamples, minorityExamples, gamma)) {
+      if(calculatePhi(spark, translated, majorityExamples, minorityExamples, gamma) < pointPhi) {
         point = translated
       }
     }
@@ -74,16 +113,15 @@ class RBOModel private[ml](override val uid: String) extends Model[RBOModel] wit
     val minorityDF = dataset.filter(dataset($(labelCol)) === minorityClassLabel)
     val majorityDF = dataset.filter(dataset($(labelCol)) =!= minorityClassLabel)
 
-    // FIXME - add parameters
-    val gamma = 1
-    val numInterations = 1 // per synthetic example
-    val stepSize = 0.01 // optimization step size
-    val p = 1.0 // probability of stopping early
-
     val minorityExamples = minorityDF.select($(featuresCol)).collect.map(x=>x(0).asInstanceOf[DenseVector].toArray)
     val majorityExamples = majorityDF.select($(featuresCol)).collect.map(x=>x(0).asInstanceOf[DenseVector].toArray)
 
-    val addedPoints = (0 until samplesToAdd).map(_=>createExample(majorityExamples, minorityExamples, gamma, stepSize, numInterations, p))
+    //val minoritySampleSize = Math.min(1000/minorityExamples.size.toDouble, 1.0)
+    //val majoritySampleSize = Math.min(1000/majorityExamples.size.toDouble, 1.0)
+
+
+    val addedPoints = (0 until samplesToAdd).map(_=>createExample(spark, majorityExamples,
+      minorityExamples, $(gamma), $(stepSize), $(iterations), $(stoppingProbability)))
 
     val foo: Array[(Double, DenseVector)] = addedPoints.map(x=>(minorityClassLabel, x)).toArray
     val bar = spark.createDataFrame(spark.sparkContext.parallelize(foo))
